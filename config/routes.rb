badb2ee62b6ea8059729eca875bfef07dac48bad
Bang Nguyen <bangnv@gmail.com>
2016-09-14 21:10:51 -0700

    create StaticPages_controller and two static pages and set the root route to StaticPages#Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/help'
+
   devise_for :users
-  root to: "home#index"
+  root to: "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
